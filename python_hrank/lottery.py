22d835ef2011edd25b98737d877f6662eef82745
EduardoKaneko <eduardockaneko@gmail.com>
2019-05-10 16:27:09 -0300

    feat: new random games

--- python_hrank/lottery.py
+++ python_hrank/lottery.py
@@ -1,6 +1,14 @@
 import random
 lottery =[]
 
-for nround in range(0, 21):
+for nround in range(0, 4):
         lottery = sorted(random.sample(range(1,61), 6))
-        print(lottery)
+        print("Jogo de 06 Números: {}".format(lottery))
+
+for nround in range(0, 2):
+        lottery = sorted(random.sample(range(1,61), 7))
+        print("Jogo de 07 Números: {}".format(lottery))
+
+for nround in range(0, 1):
+        lottery = sorted(random.sample(range(1,61), 8))
+        print("Jogo de 08 Números: {}".format(lottery))
